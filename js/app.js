3ddc226da8c6b9d1d88135a4404012ac6c18c4c8
asmtanjil <asm.tanjil@gmail.com>
2022-09-04 11:17:28 +0600

    Card Detail Added

--- js/app.js
+++ js/app.js
@@ -60,7 +60,43 @@ const displayNews = newsCategories =>{
     newCardDiv.classList.add('card');
     newCardDiv.classList.add('mb-3');
     newCardDiv.innerHTML = `
-    
+    <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${newsCategory.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title">${newsCategory.title}</h4>
+        <p class="card-text">${newsCategory.details.length > 300 ? newsCategory.details.slice(0, 300) + '...' : newsCategory.details}</p>
+        <div class="d-flex justify-content-between">
+          <div class="d-flex flex-start">
+            <img style="width: 0px; border-radius:50%;" src="${newsCategory.author.img ? newsCategory.author.img : 'No Photo Found'}" class="img-fluid" alt="">
+            <div class="ps-2 text-muted d-dlex flex-column align-items center">
+              <p><small>Name: ${newsCategory.author?.name ? newsCategory.author?.name : 'No Name Found'}</small></p>
+              <p><small class="text-wrap">
+              ${newsCategory.author?.published_date ? newsCategory.author?.published_date : 'No Date Found'}
+              </small></p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <p><i class="bi bi-eye"></i>
+              ${newsCategory.total_view ? newsCategory.total_view : 'No Data Found'}
+              </p>
+            </div>
+          </div>
+          <div>
+            <p>${newsCategory.rating.number ? newsCategory.rating.number : 'Not Rated Yet'}<i class="bi bi-star-fill"></i></p>
+          </div>
+          <div>
+            <button type="button" class="btn btn-primary text-white" data-bs-toggle="moadal" data-bs-target="#exampleModal" onclick="loadDetails('${newsCategory._id}')">
+            <i class="bi bi-arrow-right"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
     `
     cardContainer.appendChild(newCardDiv);
   })
